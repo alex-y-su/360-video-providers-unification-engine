5e676b0ee11a3002ef7ce8a6d837b9a8e5c1fa25
e.sergienko <sergienko88@ya.ru>
2020-07-08 17:07:33 +0300

    fix: try get image url from not only scr attribute

--- UnifyEngine/FulldiveVRVideoProvidersUnifyEngineTests/Implementation/DocumentInterface.cs
+++ UnifyEngine/FulldiveVRVideoProvidersUnifyEngineTests/Implementation/DocumentInterface.cs
@@ -10,6 +10,7 @@ namespace FulldiveVRVideoProvidersUnifyEngineTests.Implementation
     class DocumentInterface: IDocumentInterface
     {
         private readonly HtmlDocument _htmlDocument;
+        private string[] _possibleImageUrlAttributeName = new string[] { "src", "data-src" };
 
         public DocumentInterface(HtmlAgilityPack.HtmlDocument htmlDocument)
         {
@@ -33,7 +34,22 @@ namespace FulldiveVRVideoProvidersUnifyEngineTests.Implementation
         {
             return this._htmlDocument.DocumentNode.CssSelect(query)
                 .Where(x => x != null && x.HasAttributes)
-                .Select(x => x.GetAttributeValue("src"));
+                .Select(x => GetUrlImageArributeValue(x, _possibleImageUrlAttributeName);
+        }
+
+        private string GetUrlImageArributeValue(HtmlNode htmlNode, string[] altAttributeNames)
+        {
+            string attributeValue = string.Empty;
+            for (int i = 0; i < altAttributeNames.Length; i++)
+            {
+                attributeValue = htmlNode.GetAttributeValue(altAttributeNames[i], string.Empty);
+                if (Uri.TryCreate(attributeValue, UriKind.Absolute, out Uri uriResult)
+                    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+                {
+                    return attributeValue;
+                }
+            }
+            return string.Empty;
         }
     }
 }
